4a6039e7428d931947679686368adaf9a53053c1
Abhishek Bagusetty <abagusetty@anl.gov>
2023-06-20 00:29:24 +0000

    fix formatting, remove const-ness from SYCL-BLAS calls

--- src/tamm/kernels/cpu_blas.cpp
+++ src/tamm/kernels/cpu_blas.cpp
@@ -3,17 +3,31 @@
 #include "ga/ga_linalg.h"
 
 template<typename T, typename T1, typename T2, typename T3>
-void tamm::kernels::cpu::blas(int m, int n, int k, const T alpha, const T2* A, int lda, const T3* B, int ldb,
-          const T beta, T1* C, int ldc) {
+void tamm::kernels::cpu::blas(int m, int n, int k, const T alpha, const T2* A, int lda, const T3* B,
+                              int ldb, const T beta, T1* C, int ldc) {
   blas::gemm(blas::Layout::RowMajor, blas::Op::NoTrans, blas::Op::NoTrans, m, n, k, alpha, A, lda,
              B, ldb, beta, C, ldc);
 }
 
 // Explicit template instantiations
-template void tamm::kernels::cpu::blas(int m, int n, int k, const double alpha, const double* A, int lda, const double* B, int ldb, const double beta, double* C, int ldc);
-template void tamm::kernels::cpu::blas(int m, int n, int k, const std::complex<double> alpha, const std::complex<double>* A, int lda, const std::complex<double>* B, int ldb, const std::complex<double> beta, std::complex<double>* C, int ldc);
-template void tamm::kernels::cpu::blas(int m, int n, int k, const float alpha, const float* A, int lda, const float* B, int ldb, const float beta, float* C, int ldc);
-template void tamm::kernels::cpu::blas(int m, int n, int k, const std::complex<float> alpha, const std::complex<float>* A, int lda, const std::complex<float>* B, int ldb, const std::complex<float> beta, std::complex<float>* C, int ldc);
+template void tamm::kernels::cpu::blas(int m, int n, int k, const double alpha, const double* A,
+                                       int lda, const double* B, int ldb, const double beta,
+                                       double* C, int ldc);
+template void tamm::kernels::cpu::blas(int m, int n, int k, const std::complex<double> alpha,
+                                       const std::complex<double>* A, int lda,
+                                       const std::complex<double>* B, int ldb,
+                                       const std::complex<double> beta, std::complex<double>* C,
+                                       int ldc);
+template void tamm::kernels::cpu::blas(int m, int n, int k, const float alpha, const float* A,
+                                       int lda, const float* B, int ldb, const float beta, float* C,
+                                       int ldc);
+template void tamm::kernels::cpu::blas(int m, int n, int k, const std::complex<float> alpha,
+                                       const std::complex<float>* A, int lda,
+                                       const std::complex<float>* B, int ldb,
+                                       const std::complex<float> beta, std::complex<float>* C,
+                                       int ldc);
 
-// blas::gemm(blas::Layout, blas::Op, blas::Op, long, long, long, double, double const*, long, double const*, long, double, double*, long);
-// blas::gemm(blas::Layout, blas::Op, blas::Op, long, long, long, std::complex<double>, std::complex<double> const*, long, std::complex<double> const*, long, std::complex<double>, std::complex<double>*, long);
+// blas::gemm(blas::Layout, blas::Op, blas::Op, long, long, long, double, double const*, long,
+// double const*, long, double, double*, long); blas::gemm(blas::Layout, blas::Op, blas::Op, long,
+// long, long, std::complex<double>, std::complex<double> const*, long, std::complex<double> const*,
+// long, std::complex<double>, std::complex<double>*, long);
